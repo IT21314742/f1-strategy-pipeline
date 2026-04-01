8b6c8170a82c6e3bded71293ce04260106f8b13c
IT21314742 <it21314742@my.sliit.lk>
2026-04-01 19:04:11 +0530

    Configure Database Design
    Configure Database Design

--- sql/schema.sql
+++ sql/schema.sql
@@ -107,4 +107,4 @@ CREATE TABLE stints (
 );
 
 -- Create indexes for performance
-CREATE INDEX idx_lap_times_race_driver ON lap_times()
+CREATE INDEX idx_lap_times_race_driver ON lap_times(race_id, driver_id);
