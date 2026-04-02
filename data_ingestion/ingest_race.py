a883591e9810bee1a8db0159da7bda889fd5c1dc
IT21314742 <it21314742@my.sliit.lk>
2026-04-02 18:11:02 +0530

    Configure Pipeline
    Configure Pipeline

--- data_ingestion/ingest_race.py
+++ data_ingestion/ingest_race.py
@@ -68,4 +68,5 @@ class F1DataPipeline:
         
         # Insert race
         self.cursor.execute("""
-                            INSERT INTO races (season_id, round_number, race_name, circuit_name,)""")
+                            INSERT INTO races (season_id, round_number, race_name, circuit_name, race_date)
+                            """)
