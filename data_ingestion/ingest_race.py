7cf2562a7a89d3bdc103479b4d835be5efefd9a3
IT21314742 <it21314742@my.sliit.lk>
2026-04-03 22:15:40 +0530

    Process drivers and results
    Process drivers and results

--- data_ingestion/ingest_race.py
+++ data_ingestion/ingest_race.py
@@ -99,4 +99,5 @@ class F1DataPipeline:
             # Ensure team exists
             team_id = self.ensure_team_exists(driver_info['TeamName'])
             
-            #Insert Driver
+            # Insert Driver
+            
