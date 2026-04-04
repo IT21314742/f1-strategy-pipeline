b242a976b1ca8e8fb006b396cce76ef3cc7f8a16
IT21314742 <it21314742@my.sliit.lk>
2026-04-04 09:06:22 +0530

    Process drivers and results
    Process drivers and results

--- data_ingestion/ingest_race.py
+++ data_ingestion/ingest_race.py
@@ -118,3 +118,4 @@ class F1DataPipeline:
             # Get Race Results
             result = race.results[race.results['Abbreviation'] == driver_code]
             if not result.empty:
+                self.cursor.execute
