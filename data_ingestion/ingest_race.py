aedf00a578235774197a9c2f10a9d83048347848
IT21314742 <it21314742@my.sliit.lk>
2026-04-04 10:02:50 +0530

    Process drivers and results
    Process drivers and results

--- data_ingestion/ingest_race.py
+++ data_ingestion/ingest_race.py
@@ -182,5 +182,6 @@ class F1DataPipeline:
                         current_stint[-1]['LapNumber'],
                         current_compound,
                         len(current_stint),
-                        sum(1['LapTime'].total_seconds() for 1 in current_stint if)
+                        sum(1['LapTime'].total_seconds() for 1 in current_stint if 1['LapTime']) / len(current_stint)
+                        
                     ))
