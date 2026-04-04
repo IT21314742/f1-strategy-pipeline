0511959f8c0795b1f3546d2ad46f9f05c6bf7a80
IT21314742 <it21314742@my.sliit.lk>
2026-04-04 09:29:45 +0530

    Process drivers and results
    Process drivers and results

--- data_ingestion/ingest_race.py
+++ data_ingestion/ingest_race.py
@@ -141,5 +141,5 @@ class F1DataPipeline:
                 lap['Driver'],
                 lap['LapNumber'],
                 lap['LapTime'].total_seconds() if lap['LapTime'] else None,
-                lap['Sector1Time']
+                lap['Sector1Time'].total_seconds()
             ))
