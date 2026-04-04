e33310ba871e53f37cd8c8790a60e579790829ad
IT21314742 <it21314742@my.sliit.lk>
2026-04-04 09:43:50 +0530

    Process drivers and results
    Process drivers and results

--- data_ingestion/ingest_race.py
+++ data_ingestion/ingest_race.py
@@ -152,4 +152,6 @@ class F1DataPipeline:
             
         if lap_data:
             execute_values(self.cursor, """
-                           INSERT INTO lap_times (race_id, driver_id, lap_number, lap_time, sector1_time, sector2_time, sector3_time, position, tire_compound, tire_life)""")
+                           INSERT INTO lap_times (race_id, driver_id, lap_number, lap_time, sector1_time, sector2_time, sector3_time, position, tire_compound, tire_life)
+                           VALUES %s
+                           """, lap_data)
