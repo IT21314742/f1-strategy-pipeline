970f7803a5b957b49b3b785dd4ed1ad6d58000eb
IT21314742 <it21314742@my.sliit.lk>
2026-04-07 09:37:59 +0530

    Configure Strategy Analysis Queries
    Configure Strategy Analysis Queries

--- sql/strategy_queries.sql
+++ sql/strategy_queries.sql
@@ -1,14 +1,13 @@
--- 1. Tire strategies (Shortest Stints)
-SELECT
-r.race_name,
-d.drover_name,
-s.tire_compound,
-COUNT(*) as stint_count,
-AVG(s.stint_length) as avg_stint_length
+SELECT 
+    r.race_name,
+    d.driver_name,
+    s.tire_compound,
+    COUNT(*) as stint_count,
+    AVG(s.stint_length) as avg_stint_length
 FROM stints s
 JOIN races r ON s.race_id = r.race_id
 JOIN drivers d ON s.driver_id = d.driver_id
-WHERE r.season_id = 2025
+WHERE r.season_id = 2023
 GROUP BY r.race_name, d.driver_name, s.tire_compound
 ORDER BY avg_stint_length ASC
 LIMIT 10;
