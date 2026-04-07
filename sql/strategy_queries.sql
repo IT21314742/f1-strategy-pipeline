f95b381ae2b41192917c72aa060a65a9b64761f7
IT21314742 <it21314742@my.sliit.lk>
2026-04-07 09:37:01 +0530

    Configure Strategy Analysis Queries
    Configure Strategy Analysis Queries

--- sql/strategy_queries.sql
+++ sql/strategy_queries.sql
@@ -9,4 +9,6 @@ FROM stints s
 JOIN races r ON s.race_id = r.race_id
 JOIN drivers d ON s.driver_id = d.driver_id
 WHERE r.season_id = 2025
-GROUP BY r.race_name, 
+GROUP BY r.race_name, d.driver_name, s.tire_compound
+ORDER BY avg_stint_length ASC
+LIMIT 10;
