c6109506969868a3b02618b1d216ba7a7face79f
IT21314742 <it21314742@my.sliit.lk>
2026-04-08 20:58:49 +0530

    Config Runner file
    Config Runner file

--- sql/run_queries.py
+++ sql/run_queries.py
@@ -23,6 +23,7 @@ JOIN races r ON s.race_id = r.race_id
 JOIN drivers d ON s.driver_id = d.driver_id
 WHERE r.season_id = 2023
 GROUP BY r.race_name, d.driver_name, s.tire_compound
+ORDER BY avg_stint_length ASC
 WHERE r.race_name, 
     """
 }
