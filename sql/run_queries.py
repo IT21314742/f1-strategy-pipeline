574d00f7810110ff2450afb4e1f9a4c197ba7520
IT21314742 <it21314742@my.sliit.lk>
2026-04-08 20:57:12 +0530

    Config Runner file
    Config Runner file

--- sql/run_queries.py
+++ sql/run_queries.py
@@ -22,6 +22,7 @@ FROM stints s
 JOIN races r ON s.race_id = r.race_id
 JOIN drivers d ON s.driver_id = d.driver_id
 WHERE r.season_id = 2023
+GROUP BY r.race_name, d.driver_name, s.tire_compound
 WHERE r.race_name, 
     """
 }
