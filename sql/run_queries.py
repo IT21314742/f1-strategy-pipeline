28b030dfec7cbe633eab8dd438b5db7cb64ae34f
IT21314742 <it21314742@my.sliit.lk>
2026-04-09 21:18:32 +0530

    Configure Query for Undercut methods
    Configure Query for Undercut methods

--- sql/run_queries.py
+++ sql/run_queries.py
@@ -40,6 +40,7 @@ WITH pit_stop_analysis AS (
     AVG(prev_position - position) as avg_position_gained
     FROM pit_stop_analysis p
     JOIN drivers d ON p.driver_id = d.driver_id
-    WHERE prev_position > position"""
+    WHERE prev_position > position
+    GROUP BY d.driver_name"""
 
 }
