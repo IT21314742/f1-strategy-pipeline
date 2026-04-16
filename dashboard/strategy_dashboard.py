89693385040dcda42ded774b1f38bc3bd910df91
IT21314742 <it21314742@my.sliit.lk>
2026-04-16 18:16:37 +0530

    Configure Strategy Dashboard
    Configure Strategy Dashboard

--- dashboard/strategy_dashboard.py
+++ dashboard/strategy_dashboard.py
@@ -207,5 +207,5 @@ with col4:
     st.metric(
         "Longest Stint",
         f"{longest_stint['driver_name']} - {longest_stint['tire_compound']}",
-        
+        f"{longest_stint['stint_length']} laps"
     )
