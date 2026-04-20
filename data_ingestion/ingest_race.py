87b63dd977aa23c4aaf969b10ae64103df59a524
IT21314742 <it21314742@my.sliit.lk>
2026-04-20 21:56:53 +0530

    Configure Source code
    Configure Source code

--- data_ingestion/ingest_race.py
+++ data_ingestion/ingest_race.py
@@ -108,7 +108,7 @@ class F1DataPipeline:
                 """
                 Get all Grand Prix names for a given season.
                 """
-                schedule
+                schedule = 
 
             # Get race result
             result = race.results[race.results["Abbreviation"] == driver_code]
